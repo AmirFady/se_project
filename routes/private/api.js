14ace98c875de47e9c9e6884927e3b772b3b0e40
Amir Fady <amirfady2004@gmail.com>
2022-12-23 20:44:33 +0200

    cleaning up

--- routes/private/api.js
+++ routes/private/api.js
@@ -3,137 +3,119 @@ const { isEmpty } = require('lodash');
 const db = require('../../connectors/db');
 const roles = require('../../constants/roles');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Register HTTP endpoint to drop course from enrollements
-  app.put('/api/v1/courses/:courseId/drop', async function(req, res) {
-    // Check if user already exists in the system
-    console.log("api called");
+  app.put('/api/v1/courses/:courseId/drop', async function (req, res) {
     try {
-      console.log("trying");
       const course = await db('se_project.enrollments')
-      .where('courseId',req.params.courseId)
-      .where('userId', req.body.userId)
-      .update('active', false);
+        .where('courseId', req.params.courseId)
+        .where('userId', req.body.userId)
+        .update('active', false);
       console.log(course);
       return res.status(200).send('dropped');
-    } catch(e){
-        console.log(e);
-        res.send("do not exist");
-        return res.status(400);
+    } catch (e) {
+      console.log(e);
+      res.send("could not drop course");
+      return res.status(400);
     };
-});
-app.post('/api/v1/faculties/transfer', async function(req, res) {
-  // Check if user already exists in the system
-  const pending = await db.select('*').from('se_project.transfer_requests').where('userId',req.body.userId).where('status', "pending");
+  });
 
-  if (!isEmpty(pending)) {
-    return res.status(400).send('you already have a pending request');
-  }
-  
-  const newrequest = {
-    userId : req.body.userId,
-    newFacultyId: req.body.newFacultyId,
-    currentFacultyId: req.body.currentFacultyId,
-  };
-  try {
-    const course = await db('se_project.transfer_requests').insert(newrequest);
-    console.log('course');
-    return res.status(200).json(course);
-  } catch (e) {
-    console.log(e.message);
-    return res.status(400).send('Could not add request');
-  }
-});
-app.post('/api/v1/transfers/:transferId', async function(req, res) {
-  // Check if user already exists in the system
-  console.log("api called");
-  try {
-    console.log("trying");
-    console.log(req.body.action);
-    console.log(req.params.transferId);
-    const update = await db('se_project.transfer_requests')
-    .where('tid',req.params.transferId)
-    .update('status',req.body.action);
-    console.log('sql run');
-    return res.status(200).json(update);
-  } catch(e){
+  app.post('/api/v1/faculties/transfer', async function (req, res) {
+    const pending = await db.select('*')
+      .from('se_project.transfer_requests')
+      .where('userId', req.body.userId)
+      .where('status', "pending");
+    if (!isEmpty(pending)) {
+      return res.status(400).send("you already have a pending request");
+    }
+    const newrequest = {
+      userId: req.body.userId,
+      newFacultyId: req.body.newFacultyId,
+      currentFacultyId: req.body.currentFacultyId,
+    };
+    try {
+      const course = await db('se_project.transfer_requests').insert(newrequest);
+      return res.status(200).json(course);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not add request");
+    }
+  });
+
+  app.post('/api/v1/transfers/:transferId', async function (req, res) {
+    try {
+      const update = await db('se_project.transfer_requests')
+        .where('tid', req.params.transferId)
+        .update('status', req.body.action);
+      return res.status(200).json(update);
+    } catch (e) {
       console.log(e);
       res.send("do not exist");
       return res.status(400);
-  };
-});
+    };
+  });
 
-app.get('/api/v1/enrollment/:courseId', async function(req, res) {
-    
-  try {
-    const enrollment = await db.select('*').from('se_project.enrollments').where('courseId', req.params.courseId);
-    return res.status(200).json(enrollment);
-  } catch (e) {
-    console.log(e.message);
+  app.get('/api/v1/enrollment/:courseId', async function (req, res) {
+    try {
+      const enrollment = await db.select('*').from('se_project.enrollments').where('courseId', req.params.courseId);
+      return res.status(200).json(enrollment);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send('Could not add request');
+    }
+  });
 
-    return res.status(400).send('Could not add request');
-  }
-});
-app.put('/api/v1/enrollment/:courseId', async function(req, res) {
-  try {
+  app.put('/api/v1/enrollment/:courseId', async function (req, res) {
+    try {
       const course = await db('se_project.enrollments')
-      .where('courseId',req.params.courseId)
-      .where('userId', req.body.userId)
-      .update('grade', req.body.grade);
-    return res.status(200);
-  } catch(e){
+        .where('courseId', req.params.courseId)
+        .where('userId', req.body.userId)
+        .update('grade', req.body.grade);
+      return res.status(200);
+    } catch (e) {
       console.log(e);
-      res.send("do not exist");
-  };
-});
+      res.send("could not update");
+    };
+  });
 
+  app.get('/api/v1/faculties/:facultyId', async function (req, res) {
+    try {
+      const courses = await db.select('*')
+        .from('se_project.courses')
+        .where('facultyId', req.params.facultyId);
+      return res.status(200).json(courses);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("could not retrieve courses");
+    }
+  });
 
-app.get('/api/v1/faculties/:facultyId', async function(req, res) {
-  console.log("api")
-   
+  app.delete('/api/v1/courses/:courseId', async function (req, res) {
+    try {
+      const courses = await db.select('*')
+        .from('se_project.courses')
+        .where('cid', req.body.courseId)
+        .delete("courses");
+      return res.status(200).json(courses);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not delete course");
+    }
+  });
 
-  try {
-    const courses = await db.select('*').from('se_project.courses').where('facultyId', req.params.facultyId);
-    return res.status(200).json(courses);
-  } catch (e) {
-    console.log(e.message);
-
-    return res.status(400).send('Could not add request');
-  }
-});
-
-app.delete('/api/v1/courses/:courseId', async function(req, res) {
-    
-  try {
-    const courses = await db.select('*').from('se_project.courses')
-    .where('cid',req.body.courseId)
-    .delete("courses");
-    return res.status(200).json(courses);
-  } catch (e) {
-    console.log(e.message);
-
-    return res.status(400).send('Could not delete course');
-  }
-});
-app.put('/api/v1/courses/:courseId', async function(req, res) {
-
-  console.log("api");
-  try {
-  console.log("trying");
-    const update = await db.select('*')
-    .from('se_project.courses')
-    .where('cid', req.params.courseId)
-    .update('course',req.body.name)
-    .update('code',req.body.code)
-    .update('credit hours',req.body.hours);
-    console.log('sql run');
-    return res.status(200).json(update);
-  
-  } catch(e){
+  app.put('/api/v1/courses/:courseId', async function (req, res) {
+    try {
+      const update = await db.select('*')
+        .from('se_project.courses')
+        .where('cid', req.params.courseId)
+        .update('course', req.body.name)
+        .update('code', req.body.code)
+        .update('credit hours', req.body.hours);
+      return res.status(200).json(update);
+    } catch (e) {
       console.log(e);
-      res.send("do not exist");
-  };
-});
+      res.send("could not update");
+    };
+  });
 
 }
--- routes/private/views.js
+++ routes/private/views.js
@@ -3,7 +3,7 @@ const db = require('../../connectors/db');
 const roles = require('../../constants/roles');
 const { getSessionToken } = require('../../utils/session');
 
-const getUser = async function(req) {
+const getUser = async function (req) {
   const sessionToken = getSessionToken(req);
   if (!sessionToken) {
     return res.status(301).redirect('/');
@@ -16,118 +16,103 @@ const getUser = async function(req) {
     .innerJoin('se_project.roles', 'se_project.users.roleId', 'se_project.roles.id')
     .innerJoin('se_project.faculties', 'se_project.users.facultyId', 'se_project.faculties.fid')
     .first();
-  
+
   console.log('user =>', user)
   user.isStudent = user.roleId === roles.student;
   user.isAdmin = user.roleId === roles.admin;
 
-  return user;  
+  return user;
 }
 
-module.exports = function(app) {
-  // Register HTTP endpoint to render /users page
-  app.get('/dashboard', async function(req, res) {
+module.exports = function (app) {
+
+  app.get('/dashboard', async function (req, res) {
     const user = await getUser(req);
     return res.render('dashboard', user);
   });
 
-  app.get('/transcripts', async function(req, res) {
+  app.get('/transcripts', async function (req, res) {
     const user = await getUser(req);
     const enrollment = await db.select('*')
-    .from('se_project.enrollments')
-    .where('userId', user.uid)
-    .innerJoin('se_project.courses', 'se_project.enrollments.courseId', 'se_project.courses.cid');
-    const grade = [await db.select('grade').from('se_project.enrollments').where('userId', user.uid)];
-    const hrs = [await db.select('').from('se_project.enrollments').where('userId', user.uid)];
-    for(const i=0; i<grade.length-1; i++){
-      if(grade[i]=='a'){
-      
-
-      }
-    }
-    return res.render('transcripts', { ...user,enrollment });
+      .from('se_project.enrollments')
+      .where('userId', user.uid)
+      .innerJoin('se_project.courses', 'se_project.enrollments.courseId', 'se_project.courses.cid');
+    return res.render('transcripts', { ...user, enrollment });
   });
 
-// Register HTTP endpoint to render /courses page
-  app.get('/transfer', async function(req, res) {
-  const user = await getUser(req);
-  const faculties = await db.select('*').from('se_project.faculties')
-  const pendingrequest = await db.select('*')
-  .from('se_project.transfer_requests')
-  .where('status', "pending")
-  .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId')
- const requests = await db.select('*')
- .from('se_project.transfer_requests')
- .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId')
-  return res.render('transfer-requests', { ...user, requests, faculties, pendingrequest });
-});
+  app.get('/transfer', async function (req, res) {
+    const user = await getUser(req);
+    const faculties = await db.select('*')
+      .from('se_project.faculties');
+    const pendingrequest = await db.select('*')
+      .from('se_project.transfer_requests')
+      .where('status', "pending")
+      .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId');
+    const requests = await db.select('*')
+      .from('se_project.transfer_requests')
+      .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId');
+    return res.render('transfer-requests', { ...user, requests, faculties, pendingrequest });
+  });
 
-  
-  // Register HTTP endpoint to render /courses page
-  app.get('/courses', async function(req, res) {
+  app.get('/courses', async function (req, res) {
     const user = await getUser(req);
     const courses = await db.select('*')
-    .from('se_project.enrollments')
-    .where('userId', user.uid)
-    .where('active',true)
-    .innerJoin('se_project.courses','se_project.courses.cid','se_project.enrollments.courseId')
+      .from('se_project.enrollments')
+      .where('userId', user.uid)
+      .where('active', true)
+      .innerJoin('se_project.courses', 'se_project.courses.cid', 'se_project.enrollments.courseId');
     return res.render('courses', { ...user, courses });
   });
 
-
-  
-  // Register HTTP endpoint to render /courses page
-  app.get('/manage/transfers', async function(req, res) {
+  app.get('/manage/transfers', async function (req, res) {
     const user = await getUser(req);
     const requests = await db.select('*')
-    .from('se_project.transfer_requests').where('status',"pending")
-    .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId')
-    .innerJoin('se_project.users','se_project.users.uid','se_project.transfer_requests.userId')
-
+      .from('se_project.transfer_requests')
+      .where('status', "pending")
+      .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId')
+      .innerJoin('se_project.users', 'se_project.users.uid', 'se_project.transfer_requests.userId');
     return res.render('manage-requests', { requests, user });
   });
 
-  // Register HTTP endpoint to render /courses page
-  app.get('/manage/grades', async function(req, res) {
+  app.get('/manage/grades', async function (req, res) {
     const user = await getUser(req);
     const courseId = req.query.courseId || false;
     const courses = await db.select('*')
-    .from('se_project.courses');
+      .from('se_project.courses');
     const enrollment_query = db.select('*').from('se_project.enrollments')
-    .innerJoin('se_project.users', 'se_project.enrollments.userId', 'se_project.users.uid');
-    if(courseId) {
-      enrollment_query.where('courseId',courseId);
+      .innerJoin('se_project.users', 'se_project.enrollments.userId', 'se_project.users.uid');
+    if (courseId) {
+      enrollment_query.where('courseId', courseId);
     };
     const enrollment = await enrollment_query;
     return res.render('manage-grades', { ...user, enrollment, courseId, courses });
   });
 
-  // Register HTTP endpoint to render /courses page
-  app.get('/manage/courses', async function(req, res) {
+  app.get('/manage/courses', async function (req, res) {
     const user = await getUser(req);
     const facultyId = req.query.facultyId || false;
     const courses = await db.select('*')
-    .from('se_project.courses')
+      .from('se_project.courses');
     const faculty = await db.select('*')
-    .from('se_project.faculties')
+      .from('se_project.faculties');
     const course_query = db.select('*').from('se_project.courses')
-    .innerJoin('se_project.faculties', 'se_project.courses.facultyId', 'se_project.faculties.fid');
-    if(facultyId) {
-      course_query.where('facultyId',facultyId);
+      .innerJoin('se_project.faculties', 'se_project.courses.facultyId', 'se_project.faculties.fid');
+    if (facultyId) {
+      course_query.where('facultyId', facultyId);
     };
     const course = await course_query;
     return res.render('manage-courses', { ...user, facultyId, course, faculty, courses });
   });
-    // Register HTTP endpoint to render /courses page
-    app.get('/manage/courses/edit', async function(req, res) {
-      const courseId = req.query.courseId;
-      const user = await getUser(req);
-      const course = await db.select('*')
+
+  app.get('/manage/courses/edit', async function (req, res) {
+    const courseId = req.query.courseId;
+    const user = await getUser(req);
+    const course = await db.select('*')
       .from('se_project.courses')
       .where("cid", courseId);
-      const faculty = await db.select('*')
+    const faculty = await db.select('*')
       .from('se_project.faculties');
-      return res.render('edit-courses', { ...user, course, faculty });
-    });
+    return res.render('edit-courses', { ...user, course, faculty });
+  });
 
 };
