7d188ec01b9ca859ed232c41ab6c6eb160ff548d
Amir Fady <amirfady2004@gmail.com>
2022-12-25 19:57:54 +0200

    done2

--- routes/private/views.js
+++ routes/private/views.js
@@ -41,7 +41,7 @@ module.exports = function (app) {
       .innerJoin('se_project.faculties', 'se_project.faculties.fid', 'se_project.transfer_requests.newFacultyId');
     const requests = await db.select('*', 'tr.userId', 'se_project.users.firstName', 'se_project.users.lastName', 'newFac.faculty as nf', 'oldFac.faculty as of', 'tr.status', 'tr.request_date')
       .from('se_project.transfer_requests as tr')
-      .where('status', "pending")
+      
       .innerJoin('se_project.users', 'se_project.users.uid', 'tr.userId')
       .innerJoin('se_project.faculties as newFac', 'newFac.fid', 'tr.newFacultyId',)
       .innerJoin('se_project.faculties as oldFac', 'oldFac.fid', 'tr.currentFacultyId',);
